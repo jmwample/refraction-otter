359e8e88da7046f6fed817755aeb98cc2d7d78ab
Jack W <jack.wampler@colorado.edu>
2020-01-21 19:32:07 -0700

    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -9,12 +9,24 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>This is my gatsby Netlify site.</p>
+    <p>We are going to try to make a rective version of the CAIDA otter plots.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <a href="refraction.network">Refraction Networking Parent Site</a>
+    <h2>Link Resources</h2>
+    <ul>
+      <li>
+        <a href="https://www.caida.org/research/topology/as_core_network/2014/">AS CORE Network Graph</a>
+      </li>
+      <li>
+        <a href="https://stats.apnic.net/vizas/index.html">APNIC AS relations visualization</a>
+      </li>
+      <li>
+        <a href="https://stat.ripe.net/special/bgplay">RIPE NCC BGPLAY visualization.</a>
+      </li>
+    </ul>
   </Layout>
 )
 
